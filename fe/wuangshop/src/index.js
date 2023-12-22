bf52d0f666d1e7e387c42c5589bc488e7dcac765
minwang03 <93901911+minwang03@users.noreply.github.com>
2023-12-22 16:06:09 +0700

    Update index.js

--- fe/wuangshop/src/index.js
+++ fe/wuangshop/src/index.js
@@ -6,6 +6,6 @@ import './index.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-   <h1>Hello</h1>
+   <h1>Hello World!!!</h1>
   </React.StrictMode>
 );
