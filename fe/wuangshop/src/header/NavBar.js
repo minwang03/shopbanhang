93aedf91dd22f2ffb7fc94392410194ec1aa92f7
minwang03 <93901911+minwang03@users.noreply.github.com>
2023-12-25 10:51:40 +0700

    a
    a

--- fe/wuangshop/src/header/NavBar.js
+++ fe/wuangshop/src/header/NavBar.js
@@ -4,19 +4,54 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import SearchBar from './SearchBar';
+import styled from 'styled-components';
+
+const HoveredNavItem = styled(Nav.Link)`
+position: relative;
+display: inline-block;
+padding: 10px 20px;
+cursor: pointer;
+margin-right: 20px;
+
+&::after {
+  content: '';
+  position: absolute;
+  left: 50%;
+  transform: translateX(-50%);
+  bottom: -2px;
+  height: 2px;
+  width: 0;
+  background-color: black;
+  transition: width 0.3s ease-in-out;
+}
+
+&:hover::after {
+  width: 100%;
+}
+`;
 
 function BasicExample() {
+  const [hoveredNavItem, setHoveredNavItem] = useState(null);
   const [dropdownVisible, setDropdownVisible] = useState(false);
   let timeoutId;
+
+  const handleMouseEnterNavItem = (index) => {
+    setHoveredNavItem(index);
+  };
+
+  const handleMouseLeaveNavItem = () => {
+    setHoveredNavItem(null);
+  };
+
   const handleMouseEnter = () => {
     setDropdownVisible(true);
-      clearTimeout(timeoutId);
+    clearTimeout(timeoutId);
   };
 
   const handleMouseLeave = () => {
     timeoutId = setTimeout(() => {
-        setDropdownVisible(false);
-      }, 100);
+      setDropdownVisible(false);
+    }, 100);
   };
 
   return (
@@ -26,8 +61,30 @@ function BasicExample() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="">Home</Nav.Link>
-            <Nav.Link href="">Link</Nav.Link>
+            <HoveredNavItem
+              href=""
+              onMouseEnter={() => handleMouseEnterNavItem(1)}
+              onMouseLeave={handleMouseLeaveNavItem}
+              className={hoveredNavItem === 1 ? 'hovered-nav-item' : ''}
+            >
+              Home
+            </HoveredNavItem>
+            <HoveredNavItem
+              href=""
+              onMouseEnter={() => handleMouseEnterNavItem(2)}
+              onMouseLeave={handleMouseLeaveNavItem}
+              className={hoveredNavItem === 2 ? 'hovered-nav-item' : ''}
+            >
+              Home1
+            </HoveredNavItem>
+            <HoveredNavItem
+              href=""
+              onMouseEnter={() => handleMouseEnterNavItem(3)}
+              onMouseLeave={handleMouseLeaveNavItem}
+              className={hoveredNavItem === 3 ? 'hovered-nav-item' : ''}
+            >
+              Home2
+            </HoveredNavItem>
           </Nav>
           <SearchBar />
           <Nav>
@@ -36,7 +93,7 @@ function BasicExample() {
               className="border rounded px-3 py-1 mt-1 mb-1"
               onMouseEnter={handleMouseEnter}
               onMouseLeave={handleMouseLeave}
-              style={{width:'75px'}}
+              style={{ width: '75px' }}
             >
               Login
             </Nav.Link>
@@ -52,7 +109,7 @@ function BasicExample() {
                 onMouseEnter={handleMouseEnter}
                 onMouseLeave={handleMouseLeave}
               >
-                <Nav.Link href="" >Thông tin cá nhân</Nav.Link>
+                <Nav.Link href="">Thông tin cá nhân</Nav.Link>
                 <Nav.Link href="">Thông tin mua hàng</Nav.Link>
                 <Nav.Link href="">Cài đặt</Nav.Link>
                 <Nav.Link href="">Đăng xuất</Nav.Link>
--- fe/wuangshop/src/index.js
+++ fe/wuangshop/src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
-import Navbar from './header/NavBar.js'
+import { createRoot } from 'react-dom/client';
+import Navbar from './header/NavBar.js';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = createRoot(document.getElementById('root'));
 root.render(
   <>
-    <Navbar/>
+    <Navbar />
   </>
 );
