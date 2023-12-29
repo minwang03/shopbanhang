1c547c3c3d60905ab9eb1b46683c69b21996299f
minwang03 <93901911+minwang03@users.noreply.github.com>
2023-12-29 16:05:47 +0700

    Update index.js

--- fe/wuangshop/src/index.js
+++ fe/wuangshop/src/index.js
@@ -3,13 +3,11 @@ import './index.css';
 import { createRoot } from 'react-dom/client';
 import Navbar from './header/NavBar.js';
 import Body1 from './body/Carousels.js'
-// import Form from './body/example'
 
 const root = createRoot(document.getElementById('root'));
 root.render(
   <>
     <Navbar />
     <Body1/>
-    {/* <Form/> */}
   </>
 );
